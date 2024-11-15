b279f06ff4a4368b5ccc4c1343dcba199a4450f8
sipneat <jake.esperson@outlook.com>
2024-11-15 11:54:49 -0800

    initial commit

---
+++ bedrock.py
@@ -0,0 +1,39 @@
+import boto3
+from botocore.exceptions import ClientError
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Put your AWS credentials in a .env file
+access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+client = boto3.client(
+    service_name="bedrock-runtime",
+    aws_access_key_id=access_key_id,
+    aws_secret_access_key=secret_access_key,
+    region_name="us-west-2",
+)
+
+model_id = "mistral.mistral-large-2407-v1:0"
+
+user_message = "Summarize AWS"
+conversation = [
+    {
+        "role": "user",
+        "content": [{"text": user_message}],
+    }
+]
+
+try:
+    response = client.converse(
+        modelId=model_id,
+        messages=conversation,
+        inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9},
+    )
+
+    response_text = response["output"]["message"]["content"][0]["text"]
+    print(response_text)
+except (ClientError, Exception) as e:
+    print(f"ERROR: {e}")
